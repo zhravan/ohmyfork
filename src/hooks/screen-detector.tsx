278627ff615af110a8c2f58d7dc114163c72e1d1
shravan20 <mrshravankumarb@gmail.com>
2025-07-23 00:07:40 +0530

    Update src/hooks/screen-detector.tsx: import * as React from "react";

---
+++ src/hooks/screen-detector.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+const TABLET_BREAKPOINT = 800;
+
+export function useScreenDetector() {
+  const [isTablet, setIsTablet] = React.useState<boolean | undefined>(undefined);
+
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${TABLET_BREAKPOINT - 1}px)`);
+    const onChange = () => {
+      setIsTablet(window.innerWidth < TABLET_BREAKPOINT);
+    };
+    mql.addEventListener("change", onChange);
+    setIsTablet(window.innerWidth < TABLET_BREAKPOINT);
+    return () => mql.removeEventListener("change", onChange);
+  }, []);
+
+  return !!isTablet;
+}
