983424690d0b7e822b27a9fc736765ee1cd7de83
shravan20 <mrshravankumarb@gmail.com>
2025-09-06 19:31:44 +0530

    feat: strip backticks

--- src/App.tsx
+++ src/App.tsx
@@ -19,20 +19,23 @@ const App = () => (
   <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
     <Toaster />
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/blogs" element={<BlogsPage />} />
-        <Route path="/blogs/:slug" element={<BlogPostPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/projects/:slug" element={<ProjectPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/bug-tales" element={<BugTalesPage />} />
-        <Route path="/newsletter" element={<NewsletterPage />} />
-        <Route path="/wiki" element={<WikiShell />} />
-        <Route path="/wiki/:slug" element={<WikiShell />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      {/* Constrain main content to 80% viewport width, centered */}
+      <div className="no-page-gutter w-[80vw] mx-auto">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/blogs" element={<BlogsPage />} />
+          <Route path="/blogs/:slug" element={<BlogPostPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/:slug" element={<ProjectPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/bug-tales" element={<BugTalesPage />} />
+          <Route path="/newsletter" element={<NewsletterPage />} />
+          <Route path="/wiki" element={<WikiShell />} />
+          <Route path="/wiki/:slug" element={<WikiShell />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
       <Footer />
     </BrowserRouter>
   </ThemeProvider>
